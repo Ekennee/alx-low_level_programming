dc2980d87ab57f6e990d2605d39c50ae79327d95
ekennee <diliogbodo@gmail.com>
2022-04-08 20:06:00 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - function begins
+*
+*Return: function ends
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+		putchar(x);
+
+	putchar('\n');
+
+	return (0);
+}
