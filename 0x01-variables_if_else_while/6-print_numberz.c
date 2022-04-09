e554e75dc7e02b28ccee5395c089f8797ed03ae1
ekennee <diliogbodo@gmail.com>
2022-04-08 19:48:38 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*
+*main - funtion begins
+*
+*Return: function ends
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar((x % 10) + 0);
+
+	putchar('\n');
+
+	return (0);
+}
