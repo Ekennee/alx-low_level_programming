de2c68b5c45dfc923684ad4676fdc404774b7f07
ekennee <diliogbodo@gmail.com>
2022-04-08 19:53:34 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
-*
 *main - funtion begins
 *
 *Return: function ends
@@ -10,7 +9,7 @@ int main(void)
 	int x;
 
 	for (x = 0; x < 10; x++)
-		putchar((x % 10) + 0);
+		putchar((x % '0') + 0);
 
 	putchar('\n');
 
