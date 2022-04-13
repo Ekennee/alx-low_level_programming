e33303030229543de0ff5b393e560f1e4d838227
ekennee <diliogbodo@gmail.com>
2022-04-12 19:33:18 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,20 +7,22 @@
 
 int print_sign(int n)
 {
+	int check;
+
 	if (n > 0)
 	{
+		check = 1;
 		_putchar('+');
-		return (1);
 	}
 	else if (n == 0)
 	{
+		check = 0;
 		_putchar('0');
-		return (0);
 	}
 	else if (n < 0);
 	{
+		check = -1;
 		_putchar('-');
-		return (-1);
 	}
-	return (0);
+	return (check);
 }
