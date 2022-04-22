6d9b73b0b8c0cd62fe914f080a16b84825b8678c
ekennee <diliogbodo@gmail.com>
2022-04-21 18:17:09 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - print in reverse
+ * 
+ * @s - pointer
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (*s[c])
+	s++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
